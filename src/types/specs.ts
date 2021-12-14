dffa9a160068da81a38638773842bbcc24ba6644
codeaid <codeaid@users.noreply.github.com>
2021-12-14 16:17:41 +0000

    Added gear specialization types

---
+++ src/types/specs.ts
@@ -0,0 +1,77 @@
+import { CategoryType } from 'types/categories';
+
+// Air specializations
+export enum AirSpecType {
+  Rocketwing = 'rocketwing',
+  Wingsuit = 'wingsuit',
+}
+
+// Bike race specializations
+export enum BikeRaceSpecType {
+  Downhill = 'downhill',
+  Road = 'road',
+}
+
+// Bike tricks specializations
+export enum BikeTricksSpecType {
+  Freeride = 'freeride',
+  Slopestyle = 'slopestyle',
+}
+
+// Snow race specializations
+export enum SnowRaceSpecType {
+  DeepSnow = 'deep snow',
+  Downhill = 'downhill ski',
+}
+
+// Snow tricks specializations
+export enum SnowTricksSpecType {
+  OffTrack = 'off track',
+  Snowpark = 'snowpark',
+}
+
+// Union of all available specialization types
+export type SpecType =
+  | AirSpecType
+  | BikeRaceSpecType
+  | BikeTricksSpecType
+  | SnowRaceSpecType
+  | SnowTricksSpecType;
+
+// Specialization entity
+type GearSpecEntity<TCategoryType extends CategoryType> = {
+  category: TCategoryType;
+  code: string;
+  description: string;
+  name: string;
+  type: CategorySpec<TCategoryType>;
+};
+
+// Category specialization entity types
+type AirSpec = GearSpecEntity<CategoryType.Air>;
+type BikeRaceSpec = GearSpecEntity<CategoryType.BikeRace>;
+type BikeTricksSpec = GearSpecEntity<CategoryType.BikeTricks>;
+type SnowRaceSpec = GearSpecEntity<CategoryType.SnowRace>;
+type SnowTricksSpec = GearSpecEntity<CategoryType.SnowTricks>;
+
+// Category specialization entity union
+export type GearSpec =
+  | AirSpec
+  | BikeRaceSpec
+  | BikeTricksSpec
+  | SnowRaceSpec
+  | SnowTricksSpec;
+
+// Type retrieving specialization enum for the specified category type
+export type CategorySpec<TCategory extends CategoryType> =
+  TCategory extends CategoryType.Air
+    ? AirSpecType
+    : TCategory extends CategoryType.BikeRace
+    ? BikeRaceSpecType
+    : TCategory extends CategoryType.BikeTricks
+    ? BikeTricksSpecType
+    : TCategory extends CategoryType.SnowRace
+    ? SnowRaceSpecType
+    : TCategory extends CategoryType.SnowTricks
+    ? SnowTricksSpecType
+    : never;
