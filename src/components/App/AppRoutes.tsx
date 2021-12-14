be1f7fb311d0f6f0ed9fbeae58fc942b68f84f71
codeaid <codeaid@users.noreply.github.com>
2021-12-14 20:25:27 +0000

    Disabled code splitting

--- src/components/App/AppRoutes.tsx
+++ src/components/App/AppRoutes.tsx
@@ -1,17 +1,15 @@
-import { lazy, Suspense, VoidFunctionComponent } from 'react';
+import { Suspense, VoidFunctionComponent } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import { LoadingOverlay } from 'components';
 import { useGearCategories } from 'hooks';
 import { getCategoryPath } from 'lib/routes';
+import AirPage from 'pages/AirPage';
+import BikesRacePage from 'pages/BikesRacePage';
+import BikesTricksPage from 'pages/BikesTricksPage';
+import PlaceholderPage from 'pages/PlaceholderPage';
+import SnowRacePage from 'pages/SnowRacePage';
+import SnowTricksPage from 'pages/SnowTricksPage';
 import { CategoryType } from 'types/categories';
-
-// Import page components using code splitting
-const AirPage = lazy(() => import('pages/AirPage'));
-const BikesRacePage = lazy(() => import('pages/BikesRacePage'));
-const BikesTricksPage = lazy(() => import('pages/BikesTricksPage'));
-const PlaceholderPage = lazy(() => import('pages/PlaceholderPage'));
-const SnowRacePage = lazy(() => import('pages/SnowRacePage'));
-const SnowTricksPage = lazy(() => import('pages/SnowTricksPage'));
 
 export const AppRoutes: VoidFunctionComponent = () => {
   // Retrieve available gear categories
