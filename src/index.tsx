9cbe1ea012f3ecc1517d31d90bcd1926e64789b6
codeaid <codeaid@users.noreply.github.com>
2021-12-14 16:50:20 +0000

    Configured application theme provider

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react';
 import { render } from 'react-dom';
+import { ThemeProvider } from 'styled-components';
 import {
   App,
   GearCategoryProvider,
@@ -7,13 +8,18 @@ import {
   GearSpecProvider,
 } from 'components';
 import { categories, gear, specs } from 'config';
+import { GlobalStyle } from 'styles/global';
+import { theme } from 'styles/theme';
 
 render(
   <StrictMode>
     <GearCategoryProvider value={categories}>
       <GearSpecProvider value={specs}>
         <GearItemProvider value={gear}>
-          <App />
+          <ThemeProvider theme={theme}>
+            <GlobalStyle />
+            <App />
+          </ThemeProvider>
         </GearItemProvider>
       </GearSpecProvider>
     </GearCategoryProvider>
