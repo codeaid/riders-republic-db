880e9ce1ef3d4d994e36e2620d3d842bad9e7a0f
codeaid <codeaid@users.noreply.github.com>
2021-12-14 16:21:40 +0000

    Defined gear brand type names

---
+++ src/config/brands.ts
@@ -0,0 +1,24 @@
+import { Brand } from 'types/brands';
+
+export const brandNames = new Map<Brand, string>([
+  [Brand.Armada, 'Armada'],
+  [Brand.Atomic, 'Atomic'],
+  [Brand.BMC, 'BMC'],
+  [Brand.Burton, 'Burton'],
+  [Brand.Canyon, 'Canyon'],
+  [Brand.Commencal, 'Commencal'],
+  [Brand.Devinci, 'Devinci'],
+  [Brand.Faction, 'Faction'],
+  [Brand.Kona, 'Kona'],
+  [Brand.Lobster, 'Lobster'],
+  [Brand.Nidecker, 'Nidecker'],
+  [Brand.Nitro, 'Nitro'],
+  [Brand.RR, 'RR'],
+  [Brand.Rossignol, 'Rossignol'],
+  [Brand.Salomon, 'Salomon'],
+  [Brand.SantaCruz, 'Santa Cruz'],
+  [Brand.Specialized, 'Specialized'],
+  [Brand.Victory, 'Victory'],
+  [Brand.YTIndustries, 'YT Industries'],
+  [Brand.Yes, 'Yes'],
+]);
