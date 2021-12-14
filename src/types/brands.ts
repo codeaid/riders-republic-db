e6ddd9b8d6008da76caec184f3c64e188e63c63a
codeaid <codeaid@users.noreply.github.com>
2021-12-14 16:18:19 +0000

    Added gear brand types

---
+++ src/types/brands.ts
@@ -0,0 +1,29 @@
+export enum Brand {
+  // Air brands
+  Victory = 'victory',
+
+  // Bicycle brands
+  BMC = 'bmc',
+  Canyon = 'canyon',
+  Commencal = 'commencal',
+  Devinci = 'devinci',
+  Kona = 'kona',
+  Specialized = 'specialized',
+  SantaCruz = 'santa-cruz',
+  YTIndustries = 'yt',
+
+  // Snow brands
+  Armada = 'armada',
+  Atomic = 'atomic',
+  Burton = 'burton',
+  Faction = 'faction',
+  Lobster = 'lobster',
+  Nidecker = 'nidecker',
+  Nitro = 'nitro',
+  Rossignol = 'rossignol',
+  Salomon = 'salomon',
+  Yes = 'yes',
+
+  // Other brands
+  RR = 'rr',
+}
