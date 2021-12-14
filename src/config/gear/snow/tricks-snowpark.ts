e28ce40b5aff8267ce0f20002e3a4cf46f413538
codeaid <codeaid@users.noreply.github.com>
2021-12-14 16:36:44 +0000

    Defined available snow tricks gear

---
+++ src/config/gear/snow/tricks-snowpark.ts
@@ -0,0 +1,93 @@
+import { Brand } from 'types/brands';
+import { CategoryType } from 'types/categories';
+import { SnowGearItem } from 'types/gear';
+import { SnowTricksSpecType } from 'types/specs';
+
+const gear: Array<SnowGearItem> = [
+  {
+    brand: Brand.Nitro,
+    category: CategoryType.SnowTricks,
+    kind: 'SB',
+    model: 'Beast',
+    score: 900,
+    spec: SnowTricksSpecType.Snowpark,
+    stats: {
+      speed: 57,
+      airRotation: 72,
+      deepSnow: 30,
+      absorption: 55,
+    },
+  },
+  {
+    brand: Brand.Armada,
+    category: CategoryType.SnowTricks,
+    kind: 'SK',
+    model: 'Edollo',
+    score: 900,
+    spec: SnowTricksSpecType.Snowpark,
+    stats: {
+      speed: 57,
+      airRotation: 72,
+      deepSnow: 30,
+      absorption: 55,
+    },
+  },
+  {
+    brand: Brand.Lobster,
+    category: CategoryType.SnowTricks,
+    kind: 'SB',
+    model: 'Lucas Beaufort Artist Edition',
+    score: 900,
+    spec: SnowTricksSpecType.Snowpark,
+    stats: {
+      speed: 54,
+      airRotation: 75,
+      deepSnow: 30,
+      absorption: 55,
+    },
+  },
+  {
+    brand: Brand.Faction,
+    category: CategoryType.SnowTricks,
+    kind: 'SK',
+    model: 'Prodigy 2.0 Kengo',
+    score: 900,
+    spec: SnowTricksSpecType.Snowpark,
+    stats: {
+      speed: 54,
+      airRotation: 75,
+      deepSnow: 30,
+      absorption: 55,
+    },
+  },
+  {
+    brand: Brand.Yes,
+    category: CategoryType.SnowTricks,
+    kind: 'SB',
+    model: 'Greats Uninc',
+    score: 805,
+    spec: SnowTricksSpecType.Snowpark,
+    stats: {
+      speed: 48,
+      airRotation: 75,
+      deepSnow: 30,
+      absorption: 55,
+    },
+  },
+  {
+    brand: Brand.Armada,
+    category: CategoryType.SnowTricks,
+    kind: 'SK',
+    model: 'ARV 86 01',
+    score: 805,
+    spec: SnowTricksSpecType.Snowpark,
+    stats: {
+      speed: 48,
+      airRotation: 75,
+      deepSnow: 30,
+      absorption: 55,
+    },
+  },
+];
+
+export default gear;
