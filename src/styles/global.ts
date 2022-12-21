a886d62dfc0d5d7a6c9e3e04c38a15e5a3932865
codeaid <codeaid@users.noreply.github.com>
2022-12-21 16:12:08 +0000

    Added scrollbar styles

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -24,4 +24,29 @@ export const GlobalStyle = createGlobalStyle`
     height: 100%;
     width: 100%;
   }
+  
+  *::-webkit-scrollbar {
+    width: 0.5rem;
+  }
+
+  *::-webkit-scrollbar-button {
+    display: none;
+    height: 0;
+    width: 0;
+  }
+
+  *::-webkit-scrollbar-corner,
+  *::-webkit-scrollbar-track {
+    background-color: transparent;
+  }
+
+  *::-webkit-scrollbar-thumb {
+    background-clip: padding-box;
+    background-color: ${({ theme }) => theme.navigation.item.active.background};
+    border-color: transparent;
+    border-radius: 0.3rem;
+    border-style: solid;
+    border-width: 0.1rem;
+    width: 0.3rem;
+  }
 `;
