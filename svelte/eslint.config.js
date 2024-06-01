7ac19b8eda04719c85fe2c206c9fc2d27dc23f90
codeaid <codeaid@users.noreply.github.com>
2024-06-01 18:30:03 +0100

    Post-installation Svelte project cleanup

--- svelte/eslint.config.js
+++ svelte/eslint.config.js
@@ -1,33 +1,92 @@
-import js from '@eslint/js';
-import ts from 'typescript-eslint';
-import svelte from 'eslint-plugin-svelte';
-import prettier from 'eslint-config-prettier';
+import configPrettier from 'eslint-config-prettier';
+import pluginImport from 'eslint-plugin-import';
+import pluginPrettier from 'eslint-plugin-prettier/recommended';
+import pluginSvelte from 'eslint-plugin-svelte';
 import globals from 'globals';
+import ts from 'typescript-eslint';
 
 /** @type {import('eslint').Linter.FlatConfig[]} */
 export default [
-	js.configs.recommended,
-	...ts.configs.recommended,
-	...svelte.configs['flat/recommended'],
-	prettier,
-	...svelte.configs['flat/prettier'],
-	{
-		languageOptions: {
-			globals: {
-				...globals.browser,
-				...globals.node
-			}
-		}
-	},
-	{
-		files: ['**/*.svelte'],
-		languageOptions: {
-			parserOptions: {
-				parser: ts.parser
-			}
-		}
-	},
-	{
-		ignores: ['build/', '.svelte-kit/', 'dist/']
-	}
+  {
+    ignores: ['.svelte-kit/', 'build/', 'dist/', '**/*.{md}'],
+  },
+
+  {
+    plugins: {
+      import: pluginImport,
+    },
+  },
+
+  ...ts.configs.recommended,
+  ...pluginSvelte.configs['flat/recommended'],
+
+  {
+    files: ['**/*.{ts,tsx}'],
+    rules: {
+      // Enforce consistent usage of type imports
+      '@typescript-eslint/consistent-type-imports': 'error',
+
+      // Ensure the `type` keyword is always outside the import braces
+      'import/consistent-type-specifier-style': ['error', 'prefer-top-level'],
+
+      // Enforce consistent import order
+      'import/order': [
+        'error',
+        {
+          'alphabetize': {
+            caseInsensitive: true,
+            order: 'asc',
+          },
+          'groups': ['builtin', 'external', 'internal', 'parent', 'sibling', 'index', 'object'],
+          'pathGroups': [
+            {
+              pattern: '{.,..}/types',
+              group: 'object',
+              position: 'after',
+            },
+            {
+              pattern: '*.{jpg,jpeg,gif,png,svg}',
+              group: 'object',
+              patternOptions: {
+                matchBase: true,
+              },
+              position: 'after',
+            },
+            {
+              pattern: '*.{css,scss}',
+              group: 'object',
+              patternOptions: {
+                matchBase: true,
+              },
+              position: 'after',
+            },
+          ],
+          'newlines-between': 'never',
+        },
+      ],
+    },
+  },
+
+  configPrettier,
+  ...pluginSvelte.configs['flat/prettier'],
+
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+  },
+
+  {
+    files: ['**/*.svelte'],
+    languageOptions: {
+      parserOptions: {
+        parser: ts.parser,
+      },
+    },
+  },
+
+  pluginPrettier,
 ];
