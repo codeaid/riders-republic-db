2c54b4dc4bc9d3ebfc9edcc5f1b583bb3c07f20e
codeaid <codeaid@users.noreply.github.com>
2024-06-01 20:50:13 +0100

    Configured GitHub page workflow

--- svelte/svelte.config.js
+++ svelte/svelte.config.js
@@ -8,6 +8,9 @@ const config = {
     alias: {
       '@/*': './src/*',
     },
+    paths: {
+      base: process.argv.includes('dev') ? '' : process.env.BASE_PATH,
+    },
   },
   preprocess: vitePreprocess(),
 };
