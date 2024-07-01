05bec6ea42f3ea65469754e1dea092356d405a02
codeaid <codeaid@users.noreply.github.com>
2024-07-01 01:07:43 +0100

    Added AppLogo component

---
+++ svelte/src/components/AppLogo.svelte
@@ -0,0 +1,10 @@
+<a href="/">
+  <img
+    alt="RR Logo"
+    class="transition-transform duration-150 hover:rotate-190 hover:scale-115 hidden md:block"
+    height="34"
+    loading="eager"
+    src="/images/logo.png"
+    width="34"
+  />
+</a>
