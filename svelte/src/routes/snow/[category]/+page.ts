720053b841833ceca1e258f9ee9df8d470cc171b
codeaid <codeaid@users.noreply.github.com>
2024-07-01 01:31:01 +0100

    Added snow category redirection page

---
+++ svelte/src/routes/snow/[category]/+page.ts
@@ -0,0 +1,21 @@
+import { error, redirect } from '@sveltejs/kit';
+import type { PageLoad } from './$types';
+import { snowCategory } from '@/config/categories';
+
+export const load: PageLoad = ({ params }) => {
+  const category = snowCategory.categories.find(c => c.slug === params.category);
+  if (!category) {
+    return error(404, {
+      message: 'Category not found',
+    });
+  }
+
+  const [specialization] = category.specializations;
+  if (!specialization) {
+    return error(404, {
+      message: 'Specialization not found',
+    });
+  }
+
+  redirect(302, `/${snowCategory.slug}/${params.category}/${specialization.slug}`);
+};
