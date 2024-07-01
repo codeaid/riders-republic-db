37975c97d1fdc5270395e4b01af8e19f1d42e0cf
codeaid <codeaid@users.noreply.github.com>
2024-07-01 01:16:49 +0100

    Updated home page placeholder

--- svelte/src/routes/+page.svelte
+++ svelte/src/routes/+page.svelte
@@ -1,2 +1,7 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  import Placeholder from '@/components/Placeholder.svelte';
+</script>
+
+<slot>
+  <Placeholder>Please select a category</Placeholder>
+</slot>
