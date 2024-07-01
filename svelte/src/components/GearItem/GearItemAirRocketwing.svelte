8ab8fcf95cf21f891e2a0cafa277b54fc7078939
codeaid <codeaid@users.noreply.github.com>
2024-07-01 01:23:48 +0100

    Added GearItem component

---
+++ svelte/src/components/GearItem/GearItemAirRocketwing.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+  import GearItem from '@/components/GearItem.svelte';
+  import GearStat from '@/components/GearStat.svelte';
+  import type { AirRocketwingGear } from '@/types/gear';
+  import type { SpecializationEntity } from '@/types/specializations';
+  import type { AirRocketwingGearStat, GearStatInfoMap } from '@/types/stats';
+
+  export let gear: AirRocketwingGear;
+  export let specialization: SpecializationEntity<string, AirRocketwingGearStat>;
+  export let stats: GearStatInfoMap<AirRocketwingGearStat>;
+</script>
+
+<GearItem {gear} {specialization}>
+  <GearStat
+    type="speed"
+    value={gear.stats.speed}
+    valueHighest={stats.speed.highest}
+    valueSecond={stats.speed.secondHighest}
+    valueLowest={stats.speed.lowest}
+  />
+  <GearStat
+    type="mobility"
+    value={gear.stats.mobility}
+    valueHighest={stats.mobility.highest}
+    valueSecond={stats.mobility.secondHighest}
+    valueLowest={stats.mobility.lowest}
+  />
+  <GearStat
+    type="engine-boost"
+    value={gear.stats['engine-boost']}
+    valueHighest={stats['engine-boost'].highest}
+    valueSecond={stats['engine-boost'].secondHighest}
+    valueLowest={stats['engine-boost'].lowest}
+  />
+</GearItem>
---
+++ svelte/src/components/GearItem/GearItemAirWingsuit.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+  import GearItem from '@/components/GearItem.svelte';
+  import GearStat from '@/components/GearStat.svelte';
+  import type { AirWingsuitGear } from '@/types/gear';
+  import type { SpecializationEntity } from '@/types/specializations';
+  import type { AirWingsuitGearStat, GearStatInfoMap } from '@/types/stats';
+
+  export let gear: AirWingsuitGear;
+  export let specialization: SpecializationEntity<string, AirWingsuitGearStat>;
+  export let stats: GearStatInfoMap<AirWingsuitGearStat>;
+</script>
+
+<GearItem {gear} {specialization}>
+  <GearStat
+    type="mobility-vertical"
+    value={gear.stats['mobility-vertical']}
+    valueHighest={stats['mobility-vertical'].highest}
+    valueSecond={stats['mobility-vertical'].secondHighest}
+    valueLowest={stats['mobility-vertical'].lowest}
+  />
+  <GearStat
+    type="mobility-horizontal"
+    value={gear.stats['mobility-horizontal']}
+    valueHighest={stats['mobility-horizontal'].highest}
+    valueSecond={stats['mobility-horizontal'].secondHighest}
+    valueLowest={stats['mobility-horizontal'].lowest}
+  />
+  <GearStat
+    type="slow-mode"
+    value={gear.stats['slow-mode']}
+    valueHighest={stats['slow-mode'].highest}
+    valueSecond={stats['slow-mode'].secondHighest}
+    valueLowest={stats['slow-mode'].lowest}
+  />
+</GearItem>
