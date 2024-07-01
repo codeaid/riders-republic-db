bbf0c77162563ed1d6601f02519fb264b139991c
codeaid <codeaid@users.noreply.github.com>
2024-07-01 01:24:51 +0100

    Added specialized gear grid components

---
+++ svelte/src/components/GearGrid/GearGridBike.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import { getSortedGear } from '$lib/gear';
+  import { getGearStatInfoMap } from '$lib/stats';
+  import GearGrid from '@/components/GearGrid.svelte';
+  import GearItemBike from '@/components/GearItem/GearItemBike.svelte';
+  import type { BikeCategory, BmxCategory } from '@/types/categories';
+  import type { GetCategorySpecializations } from '@/types/utils';
+
+  export let specialization: GetCategorySpecializations<BikeCategory | BmxCategory>;
+
+  $: stats = getGearStatInfoMap(specialization.gear);
+</script>
+
+<GearGrid>
+  {#each getSortedGear(specialization.gear) as gear}
+    <GearItemBike {gear} {specialization} {stats} />
+  {/each}
+</GearGrid>
