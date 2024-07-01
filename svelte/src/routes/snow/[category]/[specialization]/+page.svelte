82af3c78b4518e6d9e867064497b364558703d3f
codeaid <codeaid@users.noreply.github.com>
2024-07-01 01:30:53 +0100

    Added snow specialization pages

---
+++ svelte/src/routes/snow/[category]/[specialization]/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import type { PageData } from './$types';
+  import CategoryTabs from '@/components/CategoryTabs.svelte';
+  import GearGridSnow from '@/components/GearGrid/GearGridSnow.svelte';
+
+  export let data: PageData;
+</script>
+
+<CategoryTabs category={data.category} parent={data.parent} specialization={data.specialization} />
+
+<GearGridSnow category={data.category} specialization={data.specialization} />
