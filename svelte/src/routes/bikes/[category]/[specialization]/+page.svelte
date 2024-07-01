2427d891b422fec0cddbb0e1b6d5fefa5b1fa6e2
codeaid <codeaid@users.noreply.github.com>
2024-07-01 01:30:15 +0100

    Added bike specialization pages

---
+++ svelte/src/routes/bikes/[category]/[specialization]/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import type { PageData } from './$types';
+  import CategoryTabs from '@/components/CategoryTabs.svelte';
+  import GearGridBike from '@/components/GearGrid/GearGridBike.svelte';
+
+  export let data: PageData;
+</script>
+
+<CategoryTabs category={data.category} parent={data.parent} specialization={data.specialization} />
+
+<GearGridBike specialization={data.specialization} />
---
+++ svelte/src/routes/bikes/[category]/[specialization]/+page.ts
@@ -0,0 +1,25 @@
+import { error } from '@sveltejs/kit';
+import type { PageLoad } from './$types';
+import { bikeCategory } from '@/config/categories';
+
+export const load: PageLoad = ({ params }) => {
+  const category = bikeCategory.categories.find(c => c.slug === params.category);
+  if (!category) {
+    return error(404, {
+      message: 'Category not found',
+    });
+  }
+
+  const specialization = category.specializations.find(s => s.slug === params.specialization);
+  if (!specialization) {
+    return error(404, {
+      message: 'Specialization not found',
+    });
+  }
+
+  return {
+    category,
+    parent: bikeCategory,
+    specialization,
+  };
+};
