c05c15b1746ae0ffbf47eaa3083202369953d51f
codeaid <codeaid@users.noreply.github.com>
2024-07-01 01:08:15 +0100

    Added Footer component

---
+++ svelte/src/components/Footer.svelte
@@ -0,0 +1,16 @@
+<footer
+  class="p-4 text-2xs leading-4 tracking-wide text-center bg-footer backdrop-blur text-black drop-shadow-sm lato-regular cursor-default z-10 bg-gradient-to-r from-footer via-white to-footer"
+>
+  <div class="max-w-screen-lg mx-auto space-y-3">
+    <p>
+      This website is not affiliated, maintained, authorized, endorsed by, or in any way officially
+      connected with Ubisoft Entertainment, or any of its subsidiaries or its affiliates. The
+      official Riders Republic™ website can be found at <a
+        class="text-blue-800 transition-colors hover:text-blue-500"
+        href="https://www.ubisoft.com/en-us/game/riders-republic"
+        >https://www.ubisoft.com/en-us/game/riders-republic</a
+      >. Riders Republic™ is a registered or unregistered trademarks of Ubisoft Entertainment in
+      the U.S. and/or other countries.
+    </p>
+  </div>
+</footer>
