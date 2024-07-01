dd73a65cde0afd02b3ec4d27cbbcb4c20d5eaaaa
codeaid <codeaid@users.noreply.github.com>
2024-07-01 01:10:15 +0100

    Added Placeholder component

---
+++ svelte/src/components/Placeholder.svelte
@@ -0,0 +1,7 @@
+<div class="w-full px-4 mt-[20vh]">
+  <div
+    class="w-full max-w-xl mx-auto bg-white text-lg shadow-md px-4 py-6 text-center select-none rounded-lg"
+  >
+    <slot />
+  </div>
+</div>
