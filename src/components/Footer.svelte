31d1b26bd81a2194f4cb544b20a674eafeccd273
codeaid <codeaid@users.noreply.github.com>
2024-07-02 03:21:20 +0100

    Added footer contact links

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -2,6 +2,18 @@
   class="p-4 text-2xs leading-4 tracking-wide text-center bg-footer backdrop-blur text-black drop-shadow-sm lato-regular cursor-default z-10 bg-gradient-to-r from-footer via-white to-footer"
 >
   <div class="max-w-screen-lg mx-auto space-y-3">
+    <p class="text-xs">
+      If you found a gear item that is missing you can <a
+        class="text-blue-800 transition-colors hover:text-blue-500"
+        href="https://discord.com/users/366350295674126337">contact me</a
+      >
+      on Discord or mention <span class="font-bold">@nekhebu</span> on the official Riders Republic
+      <a
+        class="text-blue-800 transition-colors hover:text-blue-500"
+        href="https://discord.gg/ridersrepublic">server</a
+      >.
+    </p>
+
     <p>
       This website is not affiliated, maintained, authorized, endorsed by, or in any way officially
       connected with Ubisoft Entertainment, or any of its subsidiaries or its affiliates. The
@@ -9,8 +21,8 @@
         class="text-blue-800 transition-colors hover:text-blue-500"
         href="https://www.ubisoft.com/en-us/game/riders-republic"
         >https://www.ubisoft.com/en-us/game/riders-republic</a
-      >. Riders Republic™ is a registered or unregistered trademarks of Ubisoft Entertainment in
-      the U.S. and/or other countries.
+      >. Riders Republic™ is a registered or unregistered trademark of Ubisoft Entertainment in the
+      U.S. and/or other countries.
     </p>
   </div>
 </footer>
