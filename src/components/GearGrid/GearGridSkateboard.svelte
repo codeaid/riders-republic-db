661b45d70a7c02805f37dbe948b68096883697a7
codeaid <codeaid@users.noreply.github.com>
2024-07-03 01:13:53 +0100

    Added skateboard grid and item components

---
+++ src/components/GearGrid/GearGridSkateboard.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import { getSortedGear } from '$lib/gear';
+  import { getGearStatInfoMap } from '$lib/stats';
+  import GearGrid from '@/components/GearGrid.svelte';
+  import GearItemSkateboard from '@/components/GearItem/GearItemSkateboard.svelte';
+  import type { SkateboardCategory } from '@/types/categories';
+  import type { GetCategorySpecializations } from '@/types/utils';
+
+  export let specialization: GetCategorySpecializations<SkateboardCategory>;
+
+  $: stats = getGearStatInfoMap(specialization.gear);
+</script>
+
+<GearGrid>
+  {#each getSortedGear(specialization.gear) as gear}
+    <GearItemSkateboard {gear} {specialization} {stats} />
+  {/each}
+</GearGrid>
