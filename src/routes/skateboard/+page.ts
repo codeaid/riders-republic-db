55db9dc2e9d10cc151d1462eab20ec0b9b5864e5
codeaid <codeaid@users.noreply.github.com>
2024-07-03 01:54:00 +0100

    Added skateboard pages

---
+++ src/routes/skateboard/+page.ts
@@ -0,0 +1,17 @@
+import { error, redirect } from '@sveltejs/kit';
+import type { PageLoad } from './$types';
+import { skateboardCategory } from '@/config/categories';
+
+// @ts-expect-error Workaround for Svelte not being able to build the route
+export const entries = () => [{}];
+
+export const load: PageLoad = () => {
+  const [specialization] = skateboardCategory.specializations;
+  if (!specialization) {
+    return error(404, {
+      message: 'Specialization not found',
+    });
+  }
+
+  redirect(302, `/${skateboardCategory.id}/${specialization.slug}`);
+};
---
+++ src/routes/skateboard/[specialization]/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import type { PageData } from './$types';
+  import CategoryTabs from '@/components/CategoryTabs.svelte';
+  import GearGridSkateboard from '@/components/GearGrid/GearGridSkateboard.svelte';
+
+  export let data: PageData;
+</script>
+
+<CategoryTabs category={data.category} specialization={data.specialization} />
+
+<GearGridSkateboard specialization={data.specialization} />
