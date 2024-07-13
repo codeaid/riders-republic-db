de730b136fd94949c2a6a18b603185f5a9ddc686
codeaid <codeaid@users.noreply.github.com>
2024-07-13 19:35:17 +0100

    Fixed active categories not being highlighted

--- src/components/Navigation.svelte
+++ src/components/Navigation.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import NavigationItem from './Navigation/NavigationItem.svelte';
+  import { base } from '$app/paths';
   import type { NavigationEntry } from '@/types/navigation';
 
   export let entries: NavigationEntry[] = [];
@@ -8,6 +9,6 @@
 
 <ul class="list-none select-none space-y-0.5 md:space-y-1">
   {#each entries as entry}
-    <NavigationItem active={pathname.startsWith(entry.path)} {entry} />
+    <NavigationItem active={pathname.startsWith(base + entry.path)} {entry} />
   {/each}
 </ul>
