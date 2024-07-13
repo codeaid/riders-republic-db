96b2a2909aa183958e56ab64d60c6f978b3c2726
codeaid <codeaid@users.noreply.github.com>
2024-07-13 01:53:13 +0100

    Made the footer notice more prominent

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -2,12 +2,12 @@
   class="p-4 text-2xs leading-4 tracking-wide text-center bg-footer backdrop-blur text-black drop-shadow-sm lato-regular cursor-default z-10 bg-gradient-to-r from-footer via-white to-footer"
 >
   <div class="max-w-screen-lg mx-auto space-y-3">
-    <p class="text-xs">
-      If you found a gear item that is missing you can <a
+    <p class="text-sm font-semibold">
+      If you have a gear item that is missing here you can <a
         class="text-blue-800 transition-colors hover:text-blue-500"
         href="https://discord.com/users/366350295674126337">contact me</a
       >
-      on Discord or mention me on the official Riders Republic
+      on Discord or mention @Nekhebu on the official Riders Republic
       <a
         class="text-blue-800 transition-colors hover:text-blue-500"
         href="https://discord.gg/ridersrepublic">server</a
