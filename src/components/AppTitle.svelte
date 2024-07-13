7804e2dcfe3ef9b51901da61434576529a62cb22
codeaid <codeaid@users.noreply.github.com>
2024-07-13 16:11:28 +0100

    Fixed invalid heading title paths

--- src/components/AppTitle.svelte
+++ src/components/AppTitle.svelte
@@ -1,4 +1,8 @@
-<a href="/">
+<script lang="ts">
+  import { base } from '$app/paths';
+</script>
+
+<a href="{base}/">
   <h1 class="p-2.5 text-xl text-white font-medium overflow-hidden text-ellipsis whitespace-nowrap">
     <slot />
   </h1>
